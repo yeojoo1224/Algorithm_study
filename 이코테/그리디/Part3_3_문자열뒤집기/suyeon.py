56599eb02a0ecdf139fc62fd2ebbd7fcca4e9b5e
yeoneeds <pink99120@gmail.com>
2022-03-03 00:18:32 +0900

    수정

--- 이코테/그리디/Part3_3_문자열뒤집기/suyeon.py
+++ 이코테/그리디/Part3_3_문자열뒤집기/suyeon.py
@@ -1,24 +1,21 @@
-line = [int(i) for i in list(input())]
-result = 0
-count0 = 0 #0으로 바뀌는 횟수
-count1 = 0 #1로 바뀌는 횟수
+s= input()
 
-if line[0]: #시작이 1이면
-    count0 += 1 #0으로 바뀜
+count0 = 0 #다 0으로 바꾸는 횟수
+count1 = 0 #다 1로 바꾸는 횟수
+
+#처음부터 뒤집고 시작하는 경우는 각 경우에 +1
+if s[0]=='0':
+    count1+=1
 else:
-    count1 += 1
+    count0+=1
 
-for i in range(len(line)-1):
-    if line[i] != line[i+1]: #다르면
-        if line[i+1]: #0->1
-            count0 += 1
-        else: #1->0
-            count1 += 1
-    #print(count0, count1)
-result = min(count1, count0)
+#길이보다 하나 더 작을때까지 비교
+for i in range(len(s)-1): 
+    if s[i]!=s[i+1]: #이웃한 수가 다를 때
+        if s[i+1]=='0': #다음 수가 0이면 1로 바꿔야 하는 횟수를 하나 증가
+            count1+=1
+        else: #다음수가 1이면 0으로 바꾸는 횟수 하나 증가
+            count0+=1
 
-print(result)
-
-
-
+print(min(count0,count1)) #전체를 다 0으로 바꾸는 횟수와 1로 바꾸는 횟수 중 작은 수를 출력
 
