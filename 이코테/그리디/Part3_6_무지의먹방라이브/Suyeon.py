789c04e43f0a87fa60dd9827ac708e09012cecfa
yeoneeds <pink99120@gmail.com>
2022-03-04 04:20:35 +0900

    muji tryed

---
+++ 이코테/그리디/Part3_6_무지의먹방라이브/Suyeon.py
@@ -0,0 +1,32 @@
+
+def solution(food_times, k):
+    length = len(food_times)
+    answer = 1
+
+    if k>=sum((food_times[v]) for v in range(length)): return -1
+
+    while k>0: 
+        for i in range(length):
+            food_times[i] = int(food_times[i])
+            if food_times[i]==0:
+                answer=answer%length +1
+                continue
+            else:
+                food_times[i]-=1
+                k-=1
+                answer= answer%length +1
+                if sum(food_times)==0:
+                    return -1
+                elif k==0:
+                    break
+
+    
+    return(answer)
+
+#f_times = list(map(int, input().split()))
+#a = int(input())
+
+#print(solution(f_times, a))
+
+    # ft= 
+    
