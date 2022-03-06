7e5520a744efa614e5c51d5d0dd132615f9773d4
yeoneeds <pink99120@gmail.com>
2022-03-07 00:48:06 +0900

    복습 주석추가

--- 이코테/그리디/Part3_2_곱하기혹은더하기/suyeon.py
+++ 이코테/그리디/Part3_2_곱하기혹은더하기/suyeon.py
@@ -4,12 +4,12 @@ import sys
 num = sys.stdin.readline().strip()
 result = 0 
 
-#문자열 길이 
+#문자열 길이 만큼 반복
 for i in range(len(num)): 
-    if result==0 or (int(num[i]))==0 or (int(num[i]))==1:
+    if result==0 or (int(num[i]))==0 or (int(num[i]))==1: #처음숫자는 +, 만약 주어진 문자가 0이거나 1이면 기존 결과 +를 해준다. 
         result += int(num[i])
     else:
-        result*=int(num[i])
+        result*=int(num[i]) #주어진 문자가 0,1이 아니면 곱해준다. 
 
 print(result)
         
