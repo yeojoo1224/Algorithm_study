8c5ff7ad80e6fe213a80a62906b9bf02e1383a31
yeojoo1224 <yeojoo1224@naver.com>
2022-03-08 18:05:11 +0900

    답지참조_여주

---
+++ 이코테/구현/Part3_8_외벽점검/Yeojoo.py
@@ -0,0 +1,30 @@
+from itertools import permutations
+
+def solution(n, weak, dist):
+    # 길이를 2배로 늘려서 원형을 일자 형태로 변형
+    length = len(weak)
+    for i in range(length):
+        weak.append(weak[i] + n)
+    answer = len(dist) + 1 # 투입할 친구 수의 최솟값을 찾아야하므로 len(dist) + 1로 초기화
+    # 0부터 length - 1까지의 위치를 각각 시작점으로 설정
+    for start in range(length):
+        # 친구를 나열하는 모든 경우의 수 각각에 대하여 확인
+        for friends in list(permutations(dist, len(dist))):
+            count = 1 # 투입할 친구의 수
+            # 해당 친구가 점검할 수 있는 마지막 위치
+            position = weak[start] + friends[count - 1]
+            # 시작점부터 모든 취약 지점을 확인 
+            for index in range(start, start + length):
+                # 점검할 수 있는 위치를 벗어나는 경우
+                if position < weak[index]:
+                    count += 1 # 새로운 친구를 투입
+                    if count > len(dist): # 더 투입이 불가능하면 종료
+                        break
+                    position = weak[index] + friends[count - 1]
+            answer = min(answer, count) # 최솟값 계산
+    if answer > len(dist):
+        return -1
+    return answer
+
+# 답지를 참조하였다,,, 쉬운 문젠줄 알았는데 시무룩
+# 원형 배열을 일렬로 늘어트리는 것은 생각했는데 배열 크기를 두개로 늘리는 생각을 하지 못해서 아쉽다
