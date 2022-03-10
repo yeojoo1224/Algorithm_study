ee5f2f6a1e3d3a41db5140fac052b78709352ab4
easyearn77 <lju8702@gmail.com>
2022-03-10 14:59:08 +0900

    DFS/BFS part2_ Jieon

---
+++ 이코테/DFS_BFS/Part2_0_음료수얼려먹기/Jieon.py
@@ -0,0 +1,36 @@
+import sys
+#정보 받기
+n,m=map(int,sys.stdin.readline().split())
+maze=[]
+for _ in range(n):
+    maze.append(list(map(int,sys.stdin.readline())))
+
+#상하좌우 움직임
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+
+def move(x,y):
+    if x<=-1 or x>=n or y<=-1 or y>=m:
+        return False
+    #현재노드 방문가능
+    if maze[x][y]==0:
+        maze[x][y]=1
+        #상하좌우 재귀 호출
+        for i in range(4):
+            move(x+dx[i],y+dy[i])
+        return True
+    else:
+        return False
+
+#음료수 채우기
+result=0
+for i in range(n):
+    for j in range(m):
+        if move(j,i):
+            result+=1
+
+print(result)
+
+#답지 참조
+#그래프와 재귀함수가 너무 오랜만이라 감이 잘 잡히지 않았다.
+#다른 문제를 풀어보며 익혀야 할 듯...
