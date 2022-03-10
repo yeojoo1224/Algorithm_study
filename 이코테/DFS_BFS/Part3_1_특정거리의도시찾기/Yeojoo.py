1aba7e5300f5625383201ef8bf5dcd10e52e19d6
yeojoo1224 <yeojoo1224@naver.com>
2022-03-10 23:44:14 +0900

    답지참조_여주

---
+++ 이코테/DFS_BFS/Part3_1_특정거리의도시찾기/Yeojoo.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+n, m, k, x = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+
+# 모든 도로 정보 입력받기
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+
+# 모든 도시에 대한 최단 거리 초기화
+distance = [-1] * (n + 1)
+distance[x] = 0 # 출발 도시까지의 거리는 0으로 설정
+
+q = deque([x])
+while(q):
+    now = q.popleft()
+    # 현재 도시에서 이동할 수 있는 모든 도시를 확인
+    for next_node in graph[now]:
+        # 아직 방문하지 않은 도시라면
+        if distance[next_node] == -1:
+            # 최단 거리 갱신
+            distance[next_node] = distance[now] + 1
+            q.append(next_node)
+
+# 최단 거리가 K인 모든 도시의 번호를 오름차순으로 출력
+check = False
+for i in range(1, n + 1):
+    if distance[i] == k:
+        print(i)
+        check = True
+
+# 만약 최단 거리가 K인 도시가 없다면 -1 출력 
+if check == False:
+    print(-1)
+
+# 문제를 딱 봤을 때 어렴풋이 bfs를 사용해야겠다는 것만 생각이 났구, 구현을 스스로 하지 못했다..
