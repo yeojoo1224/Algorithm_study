26ec6ac488a21d40585f43db22afdf35757987da
easyearn77 <lju8702@gmail.com>
2022-03-11 13:03:37 +0900

    (답지안봄)jieon_part3-1

---
+++ 이코테/DFS_BFS/Part3_1_특정거리의도시찾기/Jieon.py
@@ -0,0 +1,40 @@
+from collections import deque
+n,m,k,x=map(int,input().split())
+graph=[[] for _ in range(n+1)] #간선정보 저장
+
+for _ in range(m): #간선 입력
+    a,b=map(int,input().split())
+    graph[a].append(b)
+
+def bfs(x,graph):
+    queue=deque() #queue생성
+    visit=[0]*(n+1) 
+    cnt=[0]*n
+    answer=[]
+    #초기 위치 저장
+    queue.append(x)
+    visit[x]=1
+    #bfs탐색
+    while queue:
+        node=queue.popleft()
+        for i in graph[node]:
+            if visit[i]==0:
+                queue.append(i)
+                visit[i]=1
+                cnt[i]=cnt[node]+1
+                if cnt[i]==k:
+                    answer.append(i)
+    return answer
+
+result=sorted(bfs(x,graph))
+if result:
+    for i in result:
+        print(i)
+else:
+    print(-1)
+
+#답지안봄, 23분 소요
+#이 문제는 간단하게 생각 할 수 있었다. 
+#가장 가까운 노드부터 탐색하면서 한 계층이 끝날 때 마다 깊이가 늘어나므로, 거리가 +1만큼 늘어난다.
+#따라서 bfs를 사용하면서 한 계층이 시작할 때 마다 거리(cnt)를 늘려주고, 새로운 노드를 만나면 queue에 저장한다.
+#그 거리가 내가 찾는 거리(k)이고, 그 거리에서 새로운 노드가 탐색된다면, 그 노드가 내가 찾는 도시가 된다.
