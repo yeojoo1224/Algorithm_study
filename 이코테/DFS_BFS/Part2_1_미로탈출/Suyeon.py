44cfca74a5209b8acd79fedec7434023b7ce5cbe
yeoneeds <pink99120@gmail.com>
2022-03-13 21:42:40 +0900

    suyeon

--- 이코테/DFS_BFS/Part2_1_미로탈출/Suyeon.py
+++ 이코테/DFS_BFS/Part2_1_미로탈출/Suyeon.py
@@ -1,17 +1,16 @@
 from collections import deque
 
-# N, M을 공백을 기준으로 구분하여 입력 받기
 n, m = map(int, input().split())
-# 2차원 리스트의 맵 정보 입력 받기
+
 graph = []
 for i in range(n):
     graph.append(list(map(int, input())))
 
-# 이동할 네 가지 방향 정의 (상, 하, 좌, 우)
+# 상, 하, 좌, 우 정의
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-# BFS 소스코드 구현
+# BFS로 탐색
 def bfs(x, y):
     # 큐(Queue) 구현을 위해 deque 라이브러리 사용
     queue = deque()
@@ -19,7 +18,7 @@ def bfs(x, y):
     # 큐가 빌 때까지 반복하기
     while queue:
         x, y = queue.popleft()
-        # 현재 위치에서 4가지 방향으로의 위치 확인
+       
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
--- 이코테/DFS_BFS/Part3_1_특정거리의도시찾기/Suyeon.py
+++ 이코테/DFS_BFS/Part3_1_특정거리의도시찾기/Suyeon.py
@@ -1,17 +1,15 @@
 from collections import deque
 
-# 도시의 개수, 도로의 개수, 거리 정보, 출발 도시 번호
 n, m, k, x = map(int, input().split())
 graph = [[] for _ in range(n + 1)]
 
-# 모든 도로 정보 입력 받기
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
 
 # 모든 도시에 대한 최단 거리 초기화
 distance = [-1] * (n + 1)
-distance[x] = 0 # 출발 도시까지의 거리는 0으로 설정
+distance[x] = 0 
 
 # 너비 우선 탐색(BFS) 수행
 q = deque([x])
