e6ceef6a379a426816374241f6ee7fecfd828bc8
yeojoo1224 <yeojoo1224@naver.com>
2022-03-30 22:30:06 +0900

    답지참조_여ㅕㅜ

---
+++ 이코테/다이나믹프로그래밍/Part2_4_효율적인화폐구성/Yeojoo.py
@@ -0,0 +1,20 @@
+n, m = map(int, input().split())
+
+money = []
+
+for _ in range(n):
+    money.append(int(input()))
+
+dp = [10001] * (m + 1)
+
+dp[0] = 0
+
+for i in range(n):
+    for j in range(money[i], m + 1):
+        if dp[j - money[i]] != 10001:
+            dp[j] = min(dp[j], dp[j - money[i]] + 1)
+
+if dp[m] == 10001:
+    print(-1)
+else:
+    print(dp[m])
