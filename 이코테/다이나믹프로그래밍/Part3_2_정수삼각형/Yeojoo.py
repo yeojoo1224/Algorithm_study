c59676667d925b3ac928f97cb00ac0cc1b499940
yeojoo1224 <yeojoo1224@naver.com>
2022-04-01 00:56:33 +0900

    답지미참조_여주

---
+++ 이코테/다이나믹프로그래밍/Part3_2_정수삼각형/Yeojoo.py
@@ -0,0 +1,23 @@
+n = int(input())
+dp = []
+
+for i in range(n):
+    dp.append(list(map(int, input().split())))
+
+for i in range(1, n):
+    for j in range(i + 1):
+        # 왼쪽 위에서 내려오는 경우
+        if j == 0:
+            up_left = 0
+        else:
+            up_left = dp[i - 1][j - 1]
+        # 바로 위에서 내려오는 경우
+        if j == i:
+            up = 0
+        else:
+            up = dp[i - 1][j]
+        
+        #최대 합을 저장
+        dp[i][j] = dp[i][j] + max(up_left, up)
+
+print(max(dp[n - 1]))
