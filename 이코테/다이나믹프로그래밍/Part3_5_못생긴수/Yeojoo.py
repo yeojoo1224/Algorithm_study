352ca5a4af1e2215189a2c131d2f6bc6f000f2de
yeojoo1224 <yeojoo1224@naver.com>
2022-04-04 19:36:02 +0900

    답지참조_여주

---
+++ 이코테/다이나믹프로그래밍/Part3_5_못생긴수/Yeojoo.py
@@ -0,0 +1,25 @@
+n = int(input())
+
+ugly = [0] * n
+ugly[0] = 1
+
+# 2배, 3배, 5배를 위한 인덱스
+i2 = i3 = i5 = 0
+# 처음에 곱셈값을 초기화
+next2, next3, next5 = 2, 3, 5
+
+for l in range(1, n):
+    # 가능한 곱셈 결과중에서 가장 작은 수를 선택
+    ugly[l] = min(next2, next3, next5)
+    # 인덱스에 따라서 곱셈 결과를 증가
+    if ugly[l] == next2:
+        i2 += 1
+        next2 = ugly[i2] * 2
+    if ugly[l] == next3:
+        i3 += 1
+        next3 = ugly[i3] * 3
+    if ugly[l] == next5:
+        i5 += 1
+        next5 = ugly[i5] * 5
+
+print(ugly[n - 1])
